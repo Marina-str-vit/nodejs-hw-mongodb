3183173a3a0e25829d1875b82b35215a7b8944ee
Marina Strukova <meribsv@gmail.com>
2025-03-22 14:21:21 +0200

    update files

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-// src/server.js
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
@@ -8,13 +6,11 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { getAllStudents, getStudentById } from './services/students.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
+const app = express();
 
-export const startServer = () => {
-  const app = express();
-
+export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
-
   app.use(
     pino({
       transport: {
@@ -23,49 +19,45 @@ export const startServer = () => {
     }),
   );
 
-  app.get('/students', async (res) => {
-    const students = await getAllStudents();
-    res.status(200).json({
-      data: students,
-    });
-  });
-
-  app.get('/students/:studentId', async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
+  app.get('/students', async (req, res) => {
+    try {
+      const students = await getAllStudents();
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found students!',
+        data: students,
       });
-      return;
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
     }
-    res.status(200).json({
-      data: student,
-    });
   });
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
+  app.get('/students/:studentId', async (req, res) => {
+    try {
+      const { studentId } = req.params;
+      const student = await getStudentById(studentId);
+      if (!student) {
+        return res.status(404).json({
+          message: 'Students not found',
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${studentId}!`,
+        data: student,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error getting contact' });
+    }
   });
 
-  app.use('*', (res, next) => {
+  app.use((req, res) => {
     res.status(404).json({
       message: 'Not found',
     });
   });
 
-  app.use((err, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
