2dce38a68689ae8ddd7cfba0ab9a9f2d6b115ce3
Marina Strukova <meribsv@gmail.com>
2025-04-22 17:59:29 +0300

    validate

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,6 +6,7 @@ import {
   patchContact,
   deleteContact,
 } from '../services/contacts.js';
+import { createContactsSchema } from '../validation/contacts.js';
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
@@ -22,6 +23,7 @@ export const getContactsByIdController = async (req, res) => {
   if (!contact) {
     throw createError(404, `Contact not found`);
   }
+
   res.status(200).json({
     status: 200,
     message: `Successfully found contact`,
@@ -30,6 +32,18 @@ export const getContactsByIdController = async (req, res) => {
 };
 
 export const addContactController = async (req, res) => {
+  const validationResult = createContactsSchema.validate(dataToValidate);
+  if (validationResult.error) {
+    console.error(validationResult.error.message);
+  } else {
+    console.log('Data is valid!');
+  }
+  // const { error } = createContactsSchema.validate(req.body);
+  // if (error) {
+  //   throw createError(404, error.message);
+  // }
+  // console.log(error);
+
   const contact = await addContact(req.body);
 
   res.status(201).json({
---
+++ src/validation/contacts.js
@@ -0,0 +1,36 @@
+import Joi from 'joy';
+import { typeList } from '../constants/contacts';
+
+export const createContactsSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  email: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string()
+    .valid(...typeList)
+    .required(),
+});
+
+// для  Patch запитів
+export const updateContactsSchema = Joi.object({
+  name: Joi.string(),
+  phoneNumber: Joi.string(),
+  email: Joi.string(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid(...typeList),
+});
