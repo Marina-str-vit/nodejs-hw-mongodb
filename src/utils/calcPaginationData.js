ba1193c44aefb4e996c87acee52dea01c76fc121
Marina Strukova <meribsv@gmail.com>
2025-04-29 11:34:47 +0300

    services/students.js

--- src/utils/calcPaginationData.js
+++ src/utils/calcPaginationData.js
@@ -1,7 +1,7 @@
 export const calcPaginationData = (count, perPage, page) => {
   const totalPages = Math.ceil(count / perPage);
-  const hasNextPage = page < totalPages;
-  const hasPreviousPage = page > 1;
+  const hasNextPage = Boolean(totalPages - page);
+  const hasPreviousPage = page !== 1;
 
   return {
     page,
