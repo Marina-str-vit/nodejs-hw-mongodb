d0f9427692b02462a845b16d5cb9bfad258db16f
Marina Strukova <meribsv@gmail.com>
2025-05-06 00:47:27 +0300

    services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -41,16 +41,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactById = async (contactId, userId) => {
-  const contact = await ContactsCollection.findOne(
-    {
-      _id: contactId,
-    },
-    {
-      userId: userId,
-    },
-  );
-  console.log(contact);
-
+  const contact = await ContactsCollection.findOne({ _id: contactId, userId });
   return contact;
 };
 
